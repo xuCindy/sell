b9add24791a3b83f36424e7cc7acb784b14fde7e
cindyXu <serinaxu@163.com>
2019-02-20 16:02:21 +0800

    外卖项目头部区域

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- @touchmove.prevent阻止浏览器原生的滚动，然后用 better-scroll 模拟滚动 -->
+  <div id="app" @touchmove.prevent>
+    <v-header :seller="seller"></v-header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import {getSeller} from 'api'
+  import VHeader from "components/v-header/v-header"
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: "app",
+    data() {
+      return {
+        seller: {}
+      }
+    },
+    created() {
+      this._getSeller()
+    },
+    methods: {
+      _getSeller() {
+        getSeller({
+          id: this.seller.id
+        }).then((seller) => {
+          this.seller = Object.assign({}, this.seller, seller)
+        })
+      }
+    },
+    components: {
+      VHeader
+    }
   }
-}
 </script>
-<style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+<style lang="stylus" scoped>
 </style>
---
+++ src/api/helpers.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+const urlMap = {
+  development: '/',
+  production: 'http://120.79.92.111:8900/'
+}
+const baseUrl = urlMap[process.env.NODE_ENV]
+const ERR_OK = 0
+
+
+export function get(url) {
+  return function (params = {}) {
+    return axios.get(baseUrl + url, {
+      params
+    }).then((res) => {
+      const {errno, data} = res.data
+      if (errno === ERR_OK) {
+        return data
+      }
+    }).catch((e) => {
+      console.log(e)
+    })
+  }
+
+}
