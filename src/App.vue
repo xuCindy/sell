2762f58a948346c705197ad184fa149fb784bb19
cindyXu <serinaxu@163.com>
2019-02-21 17:39:53 +0800

    tab组件

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,52 @@
   <!-- @touchmove.prevent阻止浏览器原生的滚动，然后用 better-scroll 模拟滚动 -->
   <div id="app" @touchmove.prevent>
     <v-header :seller="seller"></v-header>
+    <div class="tab-wrapper">
+      <tab :tabs="tabs"></tab>
+    </div>
   </div>
 </template>
 
 <script>
   import { getSeller } from 'api'
   import VHeader from 'components/v-header/v-header'
+  import Goods from 'components/goods/goods'
+  import Ratings from 'components/ratings/ratings'
+  import Seller from 'components/seller/seller'
+  import Tab from 'components/tab/tab'
 
   export default {
     name: 'app',
     data() {
       return {
         seller: {}
+      }
+    },
+    computed: {
+      tabs() {
+        return [
+          {
+            label: '商品',
+            component: Goods,
+            data: {
+              seller: this.seller
+            }
+          },
+          {
+            label: '评论',
+            component: Ratings,
+            data: {
+              seller: this.seller
+            }
+          },
+          {
+            label: '商家',
+            component: Seller,
+            data: {
+              seller: this.seller
+            }
+          }
+        ]
       }
     },
     created() {
@@ -29,9 +63,18 @@
       }
     },
     components: {
-      VHeader
+      VHeader,
+      Tab
     }
   }
 </script>
+
 <style lang="stylus" scoped>
+  #app
+    .tab-wrapper
+      position: fixed
+      top: 136px
+      left: 0
+      right: 0
+      bottom: 0
 </style>
